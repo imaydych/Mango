d90eedc2f48944de0652457cd866c1ccd6cf4c23
imaydych <ivan.maydych@gmail.com>
2019-10-31 18:51:50 +0100

    add autorization - login/loguot, registration

--- djangomango/mango_root/mango/urls.py
+++ djangomango/mango_root/mango/urls.py
@@ -1,12 +1,29 @@
-from django.urls import path, re_path
+from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
 from . import views
-from .apiviews import about_list, about_detail
+
+
+router = routers.DefaultRouter()
+router.register(r'MangoAbout', views.MangoAboutViewSet)
+
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 
 urlpatterns = [
+    # static pages
     path('', views.search, name='search'),
     path('addproject/', views.addproject, name='addproject'),
     path('instructions/', views.instruction, name='instruction'),
     path('about/', views.add_about, name='about_list'),
-#    path("about/", about_list().as_view(), name="about_list"),
-#    path("about/<int:pk>/", about_detail().as_view(), name="about_detail")
+    #path('', include(router.urls)),
+    # API list and objects
+    path('MangoAbout/', views.ListMangoAbout.as_view()),
+    path('MangoAbout/<int:pk>/', views.DetailMangoAbout.as_view()),
+    # login/logout/registration
+    path('', include('rest_auth.urls')),
+    path('registration/', include('rest_auth.registration.urls')),
 ]
--- djangomango/mango_root/mango/views.py
+++ djangomango/mango_root/mango/views.py
@@ -1,32 +1,23 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
 from rest_framework import viewsets
-from .forms import AboutForm
-from django.http import JsonResponse
+from .serializers import MangoAboutSerializer, UserSerializer, GroupSerializer
 from .models import MangoAbout
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework import generics
+from django.contrib.auth.models import User, Group
+# from dry_rest_permissions.generics import DRYPermissions
 
 
 # Create your views here.
 
 
-def add_about(request):
-    submitted = False
-    if request.method == 'POST':
-        form = AboutForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/about/?submitted=True')
-    else:
-        form = AboutForm()
-        if 'submitted' in request.GET:
-            submitted = True
-
-    return render(request, 'about.html',
-        {'form': form, 'thank you for submittin our form': submitted})
-
-
+# Static views
 def instruction(request):
     return render(request, 'Instructions.html')
+
+
+def add_about(request):
+    return render(request, 'about.html')
 
 
 def search(request):
@@ -37,20 +28,39 @@ def addproject(request):
     return render(request, 'AddProject.html')
 
 
-# class MangoViewSet(viewsets.ModelViewSet):
-#     queryset = MangoAbout.objects.all()
+class MangoAboutViewSet(viewsets.ModelViewSet):
+    queryset = MangoAbout.objects.all()
+    serializer_class = MangoAboutSerializer
+    filter_backends = (SearchFilter, OrderingFilter)
+#    MangoAbout.owner = request.user
+    search_fields = ['id', 'name', 'email', 'message', 'inserted_timestamp']
 
-# def about_list(request):
-#     MAX_OBJECTS = 20
-#     mango = MangoAbout.objects.all()[:MAX_OBJECTS]
-#     data = {"results": list(MangoAbout.values("name", "email", "message"))}
-#     return JsonResponse(data)
-#
-#
-# def about_detail(request, pk):
-#     mango = get_object_or_404(MangoAbout, pk=pk)
-#     data = {"results": {
-#         "email": MangoAbout.name,
-#         "message": MangoAbout.message,
-#     }}
-#     return JsonResponse(data)
+
+# try autorize
+class ListMangoAbout(generics.ListCreateAPIView):
+    queryset = MangoAbout.objects.all()
+    serializer_class = MangoAboutSerializer
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ['id', 'name', 'email', 'message', 'inserted_timestamp']
+
+
+class DetailMangoAbout(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MangoAbout.objects.all()
+    serializer_class = MangoAboutSerializer
+
+
+# users
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
